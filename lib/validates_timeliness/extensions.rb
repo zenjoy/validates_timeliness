e62f85b3324f7ed535e2db7860c80ab6f845cf95
Peter Dedene <peter@dedene.com>
2013-08-29 13:15:06 +0200

    Removing ActiveRecord dependency
    for users of Mongoid

--- lib/validates_timeliness/extensions.rb
+++ lib/validates_timeliness/extensions.rb
@@ -1,7 +1,7 @@
 module ValidatesTimeliness
   module Extensions
     autoload :DateTimeSelect,         'validates_timeliness/extensions/date_time_select'
-    if ActiveRecord::VERSION::MAJOR < 4
+    if ::ActiveSupport::VERSION::MAJOR < 4
       autoload :MultiparameterHandler, 'validates_timeliness/extensions/multiparameter_handler'
     else
       autoload :AttributeAssignment,     'validates_timeliness/extensions/attribute_assignment'
@@ -11,7 +11,7 @@ module ValidatesTimeliness
 
   def self.enable_date_time_select_extension!
 
-    if ActiveRecord::VERSION::MAJOR < 4
+    if ::ActiveSupport::VERSION::MAJOR < 4
       ::ActionView::Helpers::InstanceTag.send(:include, ValidatesTimeliness::Extensions::DateTimeSelect)
     else
       ::ActionView::Helpers::Tags::DateSelect.send(:include, ValidatesTimeliness::Extensions::DateTimeSelect)
@@ -19,11 +19,11 @@ module ValidatesTimeliness
   end
 
   def self.enable_multiparameter_extension!
-    if ActiveRecord::VERSION::MAJOR < 4
+    if ::ActiveSupport::VERSION::MAJOR < 4
       ::ActiveRecord::Base.send(:include, ValidatesTimeliness::Extensions::MultiparameterHandler)
     else
-      ::ActiveRecord::Base.send(:include, ValidatesTimeliness::Extensions::AttributeAssignment)
-      ::ActiveRecord::AttributeAssignment::MultiparameterAttribute.send(:include, ValidatesTimeliness::Extensions::MultiparameterAttribute)
+      ::ActiveSupport::Base.send(:include, ValidatesTimeliness::Extensions::AttributeAssignment)
+      ::ActiveSupport::AttributeAssignment::MultiparameterAttribute.send(:include, ValidatesTimeliness::Extensions::MultiparameterAttribute)
     end
   end
 end
